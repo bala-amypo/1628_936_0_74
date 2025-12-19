84398f2e4a27204d8827ec9eb753e6b606f709cf
bala-amypo <bala@amypo.in>
2025-12-19 06:24:53 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -15,44 +15,45 @@ public class StudentEntity{
       private String email;
       private String password;
       private Date createAt;
-      public Date getCreateAt(){
-        return createAt;
-      }
-      public void setCreateAt(Date createAt){
-        this.createAt=createAt;
-      }
-      public Integer getId(){
-        return id;
-      }
-      public void setId(Integer id){
-        this.id=id;
-      }
-      public String getName(){
-        return name;
-      }
-      public void setName(String name){
-        this.name=name;
-      }
-      public String getEmail(){
-        return email;
-      }
-      public void setEmail(String email){
-        this.email=email;
-      }
-      public String getPassword(){
-        return password;
-      }
-      public void setPassword(String password){
-        this.password=password;
-      }
-      public StudentEntity(Integer id,String name,String email,String password,Date createAt){
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.password=password;
-        this.createAt=createAt;
-      }
-      public StudentEntity(){
+      // public Date getCreateAt(){
+      //   return createAt;
+      // }
+      // public void setCreateAt(Date createAt){
+      //   this.createAt=createAt;
+      // }
+      // public Integer getId(){
+      //   return id;
+      // }
+      // public void setId(Integer id){
+      //   this.id=id;
+      // }
+      // public String getName(){
+      //   return name;
+      // }
+      // public void setName(String name){
+      //   this.name=name;
+      // }
+      // public String getEmail(){
+      //   return email;
+      // }
+      // public void setEmail(String email){
+      //   this.email=email;
+      // }
+      // public String getPassword(){
+      //   return password;
+      // }
+      // public void setPassword(String password){
+      //   this.password=password;
+      // }
+      // public StudentEntity(Integer id,String name,String email,String password,Date createAt){
+      //   this.id=id;
+      //   this.name=name;
+      //   this.email=email;
+      //   this.password=password;
+      //   this.createAt=createAt;
+      // }
+      // public StudentEntity(){
 
-      }
+      // }
+      
 }
